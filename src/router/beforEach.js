718592ee641732b7ad3311d7fa9d1c2a6e0cfb20
czp709 <1456844722@qq.com>
2022-03-13 22:40:14 +0800

    minor:优化路由拦截

--- src/router/beforEach.js
+++ src/router/beforEach.js
@@ -1,26 +1,29 @@
 import store from '../store/index'
 import Cookies from "js-cookie"
 const whiteList = ['/retrievePassword', '/']
-const beforeEach = ((to, from, next,router) => {
-    if (Cookies.get('token') && sessionStorage.getItem("userInfo")) { // 判断是否已经登陆过
-      // 如果登陆过但是路由还未生成就先生成路由
-      if (whiteList.indexOf(to.path) == -1 && to.matched.length===0) {
-        const data = JSON.parse(sessionStorage.getItem('userInfo'))
-        store.dispatch('user/saveUserInfo', data).then(() => {
-          let route = store.getters["user/addRouters"][0]
-          router.addRoute(route) // 生成可访问的路由表,动态添加可访问路由表
-          next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 
-        })
-      }
-      // 如果已经登陆过，但是要前往白名单页面就清除掉登录信息
-      if(whiteList.indexOf(to.path) !== -1&&whiteList.indexOf(from.path)==-1){
-        store.dispatch("user/logout")
-      }
-      next()
-    } else {
-      // 如果没有登陆过就将除了白名单页面之外的页面访问都重定向到登陆页面
-      whiteList.indexOf(to.path) !== -1 ? next() : next('/')
+const beforeEach = ((to, from, next, router) => {
+  if (Cookies.get('token') && sessionStorage.getItem("userInfo")) { // 判断是否已经登陆过
+    // 如果登陆过但是路由还未生成就先生成路由
+    if (whiteList.indexOf(to.path) == -1 && to.matched.length === 0) {
+      const data = JSON.parse(sessionStorage.getItem('userInfo'))
+      store.dispatch('user/saveUserInfo', data).then(() => {
+        let route = store.getters["user/addRouters"][0]
+        router.addRoute(route) // 生成可访问的路由表,动态添加可访问路由表
+        next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 
+      })
     }
+    // 如果已经登陆过，但是要前往白名单页面就清除掉登录信息
+    if (whiteList.indexOf(to.path) !== -1 && whiteList.indexOf(from.path) == -1) {
+      store.dispatch("user/logout")
+    }
+    Selfexecution()
+    next()
+  } else {
+    // 如果没有登陆过就将除了白名单页面之外的页面访问都重定向到登陆页面
+    whiteList.indexOf(to.path) !== -1 ? next() : next('/')
+  }
 })
-
+const Selfexecution = (() => {
+  // 你可以在这个地方加入登陆后的其它的路由拦截，例如没有执行某个操作不能进入含有权限的某个页面等
+})()
 export default beforeEach
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 // import Vue from 'vue'
 import VueRouter from 'vue-router'
-import login from '@/components/login'
-import retrievePassword from '@/components/retrievePassword'
-import {levelRouters} from './levelRouters/index'
+const login = () => import('@/components/login')
+const retrievePassword = () => import('@/components/retrievePassword')
+import { levelRouters } from './levelRouters/index'
 import beforEach from '@/router/beforEach'
 Vue.use(VueRouter)
 
@@ -30,10 +30,10 @@ const createRouter = () => new VueRouter({
 const router = createRouter()
 // 重置路由（由于router2没有提供重置路由的方式，所以采用替换路由的方式）
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+    const newRouter = createRouter()
+    router.matcher = newRouter.matcher // reset router
 }
 // 路由守卫
-router.beforeEach((to, from, next) => beforEach(to, from, next,router));
+router.beforeEach((to, from, next) => beforEach(to, from, next, router));
 
 export default router
